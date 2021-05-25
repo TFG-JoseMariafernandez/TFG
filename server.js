90c584cfcbb2a4d8e8c4ecd9e2511d5af4b9f96b
pepefer97 <pepefernandez97@gmail.com>
2021-05-25 17:30:48 +0200

    cambios para heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/TFG'));
+app.use(express.static(__dirname + '/dist/tfg-nuevo'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/TFG/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/tfg-nuevo/index.html'));
 });
 
 // default Heroku port
