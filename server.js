fe3537b6758911890eac2d3a92156a149e7d1aeb
pepefer97 <pepefernandez97@gmail.com>
2021-05-25 17:17:01 +0200

    Cambios heroku

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/tfg-nuevo/src'));
+app.use(express.static(__dirname + '/dist/TFG'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/tfg-nuevo/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/TFG/index.html'));
 });
 
 // default Heroku port
-const server = app.listen(process.env.PORT || 5000, () => {
-  const port = server.address().port;
-  console.log(`Express is working on port ${port}`);
-});
+app.listen(process.env.PORT || 5000);
